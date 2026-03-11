938306420fd942eef0624e5bbdb9d940198b2ae1
filgut00 <filgutorov@gmail.com>
2026-03-11 18:04:35 +0300

    camera OOP updates 1.0.2

--- Camera.cpp
+++ Camera.cpp
@@ -1,30 +1,20 @@
 #include "Camera.h"
-#include <iostream>
-
 
 Camera::Camera() {
-
     cap.open(0);
-
-
     if (!cap.isOpened()) {
-        std::cerr << "ОШИБКА: Не удалось открыть камеру!" << std::endl;
-    } else {
-        std::cout << "Камера успешно запущена!" << std::endl;
+        throw CameraException("Failed to open camera");
     }
 }
 
-Camera::~Camera() {
 
+
+Camera::~Camera() {
     cap.release();
-    std::cout << "Камера выключена." << std::endl;
 }
 
 cv::Mat Camera::getFrame() {
     cv::Mat frame;
-
-
     cap.read(frame);
-
     return frame;
 }
