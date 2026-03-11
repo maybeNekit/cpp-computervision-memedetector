a0735ecfc9723e099c60930a13a42c87faf5f23e
filgut00 <filgutorov@gmail.com>
2026-03-11 18:03:56 +0300

    OOP camera updates 1.0.1

--- Camera.h
+++ Camera.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <opencv2/opencv.hpp>
+#include <stdexcept>
+#include <string>
+
+class CameraException : public std::runtime_error {
+public:
+    explicit CameraException(const std::string& msg) : std::runtime_error(msg) {}
+};
 
 class Camera {
 private:
     cv::VideoCapture cap;
+
+
 public:
     Camera();
     ~Camera();
